1cf13e4dd740f57e751672aa205d4d8358b74eef
tyviroth.planb <tyviroth.planb@gmail.com>
2021-05-30 19:00:40 +0700

    First commit

---
+++ app/src/main/java/com/samnong/app/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.samnong.app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.samnong.app.databinding.ActivityMainBinding
+
+class MainActivity : AppActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.navView.setupWithNavController(navController = navController)
+        binding.drawerLayout.addDrawerListener(object : ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close) {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                super.onDrawerSlide(drawerView, slideOffset)
+                val slideX = drawerView.width * slideOffset
+                binding.navHostFragment.translationX = slideX
+            }
+        })
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.displayName != "com.samnong.app:id/firstFragment") {
+                binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            } else {
+                binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            }
+        }
+
+    }
+
+
+}
