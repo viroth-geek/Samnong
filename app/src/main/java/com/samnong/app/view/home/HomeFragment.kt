fa49b15566d1b8b66c601abbad0c00b4d2dabc48
tyviroth.planb <tyviroth.planb@gmail.com>
2021-06-12 14:25:35 +0700

    Delete use home container fragment

--- app/src/main/java/com/samnong/app/view/home/HomeFragment.kt
+++ app/src/main/java/com/samnong/app/view/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -17,7 +16,6 @@ import com.samnong.app.R
 import com.samnong.app.databinding.FragmentHomeBinding
 import com.samnong.app.epoxy.controller.CategoryController
 import com.samnong.app.model.CategoryElement
-import com.samnong.app.view.order.OrderedFragment
 import com.seanghay.statusbar.statusBar
 
 class HomeFragment : Fragment() {
@@ -49,7 +47,7 @@ class HomeFragment : Fragment() {
         controller = CategoryController(viewModel= viewModel, context = requireContext(), object : ItemClickListener {
             override fun onProductCategoryItemClick(categoryElement: CategoryElement) {
                 super.onProductCategoryItemClick(categoryElement)
-                replaceFragmentOfTag("home_fragment") { OrderedFragment.newInstance() }
+                findNavController().navigate(R.id.action_firstFragment_to_messageFragment)
             }
         })
         binding.icMenu.setOnClickListener {
@@ -57,7 +55,6 @@ class HomeFragment : Fragment() {
                 .openDrawer(GravityCompat.START)
         }
         binding.icSearch.setOnClickListener {
-//            findNavController().navigate(R.id.action_firstFragment_to_messageFragment)
         }
 
         binding.recyclerView.setController(controller = controller)
@@ -74,18 +71,5 @@ class HomeFragment : Fragment() {
         viewModel.categories.observe(viewLifecycleOwner, {
 
         })
-    }
-    private fun <T : Fragment> replaceFragmentOfTag(tag: String, factory: () -> T) {
-        val primaryFragment = findFragmentByTagOrElse(tag, factory)
-        childFragmentManager.commit {
-            setReorderingAllowed(true)
-            replace(R.id.fragment_container, primaryFragment, tag)
-            addToBackStack(primaryFragment::class.java.name)
-        }
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    private fun <T : Fragment> findFragmentByTagOrElse(tag: String, factory: () -> T): T {
-        return childFragmentManager.findFragmentByTag(tag) as? T ?: factory()
     }
 }
